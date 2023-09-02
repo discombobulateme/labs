5c78c6bbdcc6aa27c6e9dee09293b942643cb8a3
discombobulateme <paloma.oliveira@saucelabs.com>
2023-09-02 10:00:28 +0200

    Add labs 8

--- ch-8/labs-1/parallel.js
+++ ch-8/labs-1/parallel.js
@@ -1,23 +1,72 @@
-const print = (err, contents) => { 
-  if (err) console.error(err)
-  else console.log(contents )
-}
+/*
+const print = (err, contents) => {
+  if (err) console.error(err);
+  else console.log(contents);
+};
 
 const opA = (cb) => {
   setTimeout(() => {
-    cb(null, 'A')
-  }, 500)
-}
+    cb(null, 'A');
+  }, 500);
+};
 
 const opB = (cb) => {
   setTimeout(() => {
-    cb(null, 'B')
-  }, 250)
-}
+    cb(null, 'B');
+  }, 250);
+};
 
 const opC = (cb) => {
   setTimeout(() => {
-    cb(null, 'C')
-  }, 125)
+    cb(null, 'C');
+  }, 125);
+};
+
+async function run() {
+  await opA(print);
+  await opB(print);
+  await opC(print);
 }
 
+run().catch(console.error);
+*/
+
+const print = (err, contents) => {
+  if (err) console.error(err);
+  else console.log(contents);
+};
+
+const opA = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('A');
+    }, 500);
+  });
+};
+
+const opB = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('B');
+    }, 250);
+  });
+};
+
+const opC = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('C');
+    }, 125);
+  });
+};
+
+(async () => {
+  const resultA = await opA();
+  print(null, resultA);
+
+  const resultB = await opB();
+  print(null, resultB);
+
+  const resultC = await opC();
+  print(null, resultC);
+})();
--- ch-8/labs-2/serial.js
+++ ch-8/labs-2/serial.js
@@ -1,9 +1,10 @@
+/*
 'use strict'
 const { promisify } = require('util')
 
-const print = (err, contents) => { 
+const print = (err, contents) => {
   if (err) console.error(err)
-  else console.log(contents) 
+  else console.log(contents)
 }
 
 const opA = (cb) => {
@@ -23,3 +24,42 @@ const opC = (cb) => {
     cb(null, 'C')
   }, 125)
 }
+
+*/
+
+'use strict';
+
+const { promisify } = require('util');
+
+const print = (err, contents) => {
+  if (err) console.error(err);
+  else console.log(contents);
+};
+
+const setTimeoutPromise = promisify(setTimeout);
+
+const opA = async () => {
+  await setTimeoutPromise(500);
+  return 'A';
+};
+
+const opB = async () => {
+  await setTimeoutPromise(250);
+  return 'B';
+};
+
+const opC = async () => {
+  await setTimeoutPromise(125);
+  return 'C';
+};
+
+(async () => {
+  const results = [];
+
+  const tasks = [opA, opB, opC];
+  for (const task of tasks) {
+    const result = await task();
+    results.push(result);
+    print(null, result);
+  }
+})();
