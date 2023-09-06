954b4312851b266466b013f2f49eb2f6a952323a
discombobulateme <paloma.oliveira@saucelabs.com>
2023-09-06 14:58:14 +0200

    Add labs 11

--- ch-11/labs-1/index.js
+++ ch-11/labs-1/index.js
@@ -1,6 +1,15 @@
+// Lab11.1-Create a Buffer Safely
+/*
+Alter the code so that the buffer is safely allocated.
+Do not explicitly fill the buffer with anything. If the
+process prints the buffer and then logs passed!, the exercise
+was correctly completed
+*/
+
 'use strict'
 const assert = require('assert')
-const buffer = Buffer.allocUnsafe(4096)
+// const buffer = Buffer.allocUnsafe(4096)
+const buffer = Buffer.alloc(4096)
 console.log(buffer)
 
 for (const byte of buffer) assert.equal(byte, 0)
--- ch-11/labs-2/index.js
+++ ch-11/labs-2/index.js
@@ -1,13 +1,25 @@
-'use strict'
-const assert = require('assert')
-const str = 'buffers are neat'
-const base64 = '' // convert str to base64
+// Lab11.2-Convert a String to base64 Encoded String by Using a Buffer
 
-console.log(base64)
+/*
+Using the Buffer API in some way, edit the code so that the
+base64 constant contains a Base64 representation of the str
+constant. If the process prints the Base64 string and then
+logs passed!, the exercise was correctly completed
+*/
 
-assert.equal(base64, Buffer.from([
-  89,110,86,109,90,109,86,121,99,
-  121,66,104,99,109,85,103,98,109,
-  86,104,100,65,61,61]))
+'use strict';
+const assert = require('assert');
+const str = 'buffers are neat';
+// const base64 = ''; // convert str to base64
+const base64 = Buffer.from(str).toString('base64');
 
-console.log('passed!')
+console.log(base64);
+
+assert.equal(
+  base64,
+  Buffer.from([
+    89, 110, 86, 109, 90, 109, 86, 121, 99, 121, 66, 104, 99, 109, 85, 103, 98, 109, 86, 104, 100, 65, 61, 61,
+  ])
+);
+
+console.log('passed!');
