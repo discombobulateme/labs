6117543f3a32243d7b1d585b7a80fdebc18a6905
discombobulateme <paloma.oliveira@saucelabs.com>
2023-09-06 14:58:20 +0200

    add labs 12

--- ch-12/labs-1/index.js
+++ ch-12/labs-1/index.js
@@ -1,29 +1,56 @@
+// Lab12.1-Piping
+
+/*
+Use the appropriate method to make sure that all data in the readable stream is
+automatically sent to the writable stream.
+If successfully implemented the process will output: passed!
+*/
+
 'use strict'
 const { Readable, Writable } = require('stream')
 const assert = require('assert')
+// creates and returns a writable stream
 const createWritable = () => {
-  const sink = []
-  let piped = false
+  const sink = []; // store the data that is written to the writable stream
+  let piped = false;
+  // run a callback function asynchronously
   setImmediate(() => {
-    assert.strictEqual(piped, true, 'use the pipe method')
-    assert.deepStrictEqual(sink, ['a', 'b', 'c'])
-  })
+    // logic to ensure that the pipe method is used, and it checks if the data written matches the expected result
+    assert.strictEqual(piped, true, 'use the pipe method');
+    assert.deepStrictEqual(sink, ['a', 'b', 'c']);
+  });
+  // create the writable stream writable using the Writable class
   const writable = new Writable({
     decodeStrings: false,
+    /* method is called for each chunk of data written to the stream.
+    It pushes the chunk into the sink array
+    and calls the provided callback cb() to signal that the write operation is complete
+    */
     write(chunk, enc, cb) {
-      sink.push(chunk)
-      cb()
+      sink.push(chunk);
+      cb();
     },
     final() {
-      console.log('passed!')
-    }
-  })
+      console.log('passed!');
+    },
+  });
+  /*
+   event listener to the writable stream to listen for the 'pipe' event.
+   When the 'pipe' event occurs, it sets piped to true
+  */
   writable.once('pipe', () => {
-    piped = true
-  })
-  return writable
+    piped = true;
+  });
+  return writable;
 }
 const readable = Readable.from(['a', 'b', 'c'])
 const writable = createWritable()
 
 // TODO - send all data from readable to writable:
+
+/*
+Send all data from readable to writable using the pipe method
+use the pipe method to direct the data from the readable stream to the writable stream.
+This is the crucial step that ensures all data from the readable stream is automatically sent to the writable stream
+*/
+readable.pipe(writable)
--- ch-12/labs-2/index.js
+++ ch-12/labs-2/index.js
@@ -1,3 +1,12 @@
+
+// Lab 12.2 - Create a Transform Stream
+
+/*
+Replace the line that states const transform = newPassThrough() so that
+transform is assigned to a transform stream that uppercases any incoming chunks.
+If successfully implemented the process will output: passed!
+*/
+
 'use strict'
 const { Readable, Writable, Transform, PassThrough, pipeline } = require('stream')
 const assert = require('assert')
@@ -15,10 +24,17 @@ const createWritable = () => {
 const readable = Readable.from(['a', 'b', 'c'])
 const writable = createWritable()
 
-// TODO: replace the pass through stream 
-// with a transform stream that uppercases
-// incoming characters
-const transform = new PassThrough()
+// TODO: replace the pass through stream with a transform stream that uppercases incoming characters
+// const transform = new PassThrough()
+
+// Create a Transform stream that uppercases incoming characters
+const transform = new Transform({
+  transform(chunk, enc, cb) {
+    const uppercasedChunk = chunk.toString().toUpperCase();
+    cb(null, uppercasedChunk); // indicate that the transformation is complete
+  }
+})
+
 
 pipeline(readable, transform, writable, (err) => {
   assert.ifError(err)
