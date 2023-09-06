f95711845762ff8d7edfc981970eff7792c82d73
discombobulateme <paloma.oliveira@saucelabs.com>
2023-09-06 14:58:07 +0200

    Add labs 10

--- ch-10/labs-1/index.js
+++ ch-10/labs-1/index.js
@@ -1,3 +1,21 @@
+// Lab10.1-Synchronous Error Handling
+
+/* The native URL constructor can be used to parse URLs, it's
+been wrapped into a function called parse URL
+
+If URL is passed a unparsable URL string it will throw, so calling
+parseURL('foo') will result in an exception
+
+Modify the parseURL function body such that instead of throwing
+an error, it returns null when the URL is invalid.
+Use the fact that URL will throw when given invalid input to
+determine whether or not to return null or a parsed object.
+
+Once implemented, execute the index.js file with node,
+if the output says passed! then the exercise was completed successfully
+*/
+
+/*
 'use strict'
 const assert = require('assert')
 
@@ -9,7 +27,27 @@ function parseUrl (str) {
 assert.doesNotThrow(() => { parseUrl('invalid-url') })
 assert.equal(parseUrl('invalid-url'), null)
 assert.deepEqual(
-  parseUrl('http://example.com'), 
+  parseUrl('http://example.com'),
   new URL('http://example.com')
 )
 console.log('passed!')
+*/
+
+'use strict';
+const assert = require('assert');
+
+function parseUrl(str) {
+  try {
+    const parsed = new URL(str);
+    return parsed;
+  } catch (error) {
+    return null;
+  }
+}
+
+assert.doesNotThrow(() => {
+  parseUrl('invalid-url');
+});
+assert.equal(parseUrl('invalid-url'), null);
+assert.deepEqual(parseUrl('http://example.com'), new URL('http://example.com'));
+console.log('passed!');
--- ch-10/labs-2/index.js
+++ ch-10/labs-2/index.js
@@ -1,3 +1,23 @@
+// Lab10.2-AsyncFunction Error Handling
+
+/*
+The promise returned from fs/promises readFile may reject
+for a variety of reasons, for instance if the specified file
+path doesn't exist or the process doesn't have permissions to
+access it. In this scenario, we don't care what the reason for
+failure is, we just want to propagate a single error instance
+from the native Error constructor with the message
+'failed to read'.
+
+Modify the body of the read function so that any possible
+rejection by the promise returned from the fs/promises readFile
+call results in the read function rejecting with a
+new Error('failed to read') error.
+If implemented correctly, when nodeindex.js is executed the output
+should be passed!
+*/
+
+/*
 'use strict'
 const fs = require('fs')
 const assert = require('assert')
@@ -10,7 +30,7 @@ async function read (file) {
 
 async function check () {
   await assert.rejects(
-    read('not-a-valid-filepath'), 
+    read('not-a-valid-filepath'),
     new Error('failed to read')
   )
   assert.deepEqual(
@@ -21,3 +41,26 @@ async function check () {
 }
 
 check()
+*/
+
+'use strict';
+const fs = require('fs');
+const assert = require('assert');
+
+async function read(file) {
+  try {
+    const content = await fs.promises.readFile(file);
+    return content;
+  } catch (error) {
+    throw new Error('failed to read');
+  }
+}
+
+async function check() {
+  await assert.rejects(read('not-a-valid-filepath'), new Error('failed to read'));
+  assert.deepEqual(await read(__filename), fs.readFileSync(__filename));
+  console.log('passed!');
+}
+
+check();
+
